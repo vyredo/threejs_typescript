0d17f801564b596b561f3e24f019a1c5754d902d
vidy.alfredo@shopee.com <vidy.alfredo@shopee.com>
2019-11-22 12:32:50 +0700

    init

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 import './style.css'
 // three.js
 import * as THREE from 'three'
+import { MeshBasicMaterial } from 'three';
 
 function init(){
 	let scene = new THREE.Scene();
@@ -9,10 +10,11 @@ function init(){
 
 		// create and locate the camera
 	
-	let camera = new THREE.PerspectiveCamera(30,
+	let camera = new THREE.PerspectiveCamera(100,
 		window.innerWidth / window.innerHeight, 
 		1, 1000);
-		camera.position.z = 5;
+		camera.position.z = 10;
+		camera.position.y = -2;
 
 	let renderer = new THREE.WebGLRenderer();
 		renderer.setSize(window.innerWidth, window.innerHeight);
@@ -24,18 +26,74 @@ function init(){
 
 let { renderer, camera, scene } = init();
 
-let mainLoop = function(){
-	type createCubeT = (x:number, y:number, z:number, color: string) => THREE.Mesh; 
-	let createCube:createCubeT = function(x:number, y:number, z:number, color: string){
-
+function generateGeometries(){
+	type createCubeT = (x:number, y:number, z:number, option:THREE.MeshBasicMaterialParameters) => THREE.Mesh; 
+	let createCube:createCubeT = function(x, y, z, option){
 		let geometry = new THREE.BoxGeometry(x,y,z);
-		let material = new THREE.MeshBasicMaterial({ color });
+		let material = new THREE.MeshBasicMaterial(option);
+		return new THREE.Mesh(geometry, material);
 	}
+
 	
-	scene.add()
+
+	const cube1 = createCube(1,1,1,{color: 0Xc9b92b});
+	cube1.position.z = -6;
+	cube1.position.y = -5; 
+
+	const cube2 = createCube(3,3,3,{color: 0Xff0040, transparent: true, opacity: 0.8})
+	cube2.position.z = 6;
+	cube2.position.y = -5; 
+
+	const plane = new THREE.Mesh(
+		new THREE.PlaneGeometry(1000,1000,50,50),
+		new THREE.MeshBasicMaterial({color: 0Xa6f995, wireframe: true})
+	);
+	plane.rotation.x = Math.PI / 2;
+	plane.position.y = -100;
+	return { cube1, cube2, plane };
+}
+
+let { cube1, cube2, plane } = generateGeometries();
+
+let state = {
+	cube1: 'left' as dir,
+	cube2: 'right' as dir
+}
+
+type dir = 'left' | 'right';
+
+const moveCubeByDirection = (cube:THREE.Mesh, dir: dir) => {
+	if(dir === 'left'){
+		cube.position.x += 0.1;
+	} else {
+		cube.position.x -= 0.1;
+	}
+	return cube;
+}
+
+let mainLoop = function(state){
+	
+	cube1 = moveCubeByDirection(cube1, state.cube1);
+	cube2 = moveCubeByDirection(cube2, state.cube2);
+	console.log('cube1', cube1.position)
+	console.log('cube2', cube2.position)
+
+	if(cube1.position.x >= 1000 || cube1.position.x <= 0){
+		state.cube1 = state.cube1 === 'left' ? 'right' : 'left';
+	} 
+	if(cube2.position.x >= 1000 || cube2.position.x <= 0) {
+		state.cube2 = state.cube2 === 'left' ? 'right' : 'left';
+	}
+
+
+
+	scene.add(plane);
+	scene.add(cube1);
+	scene.add(cube2);
+
 	renderer.render(scene, camera);
-	requestAnimationFrame(mainLoop);	
+	requestAnimationFrame(mainLoop.bind(null,state));	
 }
 
 init();
-mainLoop();
+mainLoop(state);
